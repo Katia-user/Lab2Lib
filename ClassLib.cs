5deef0bfc5db70192c8e5f269fd9888caf8e2886
Katia <katia.rdk1298@gmail.com>
2025-09-27 21:40:16 +0300

    Add project files.

---
+++ ClassLib.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2Lib
+{
+    public class ClassLib
+    {
+        class Processor
+        {
+            public string Brand;
+            public string Model;
+            public int Generation;
+            public double Frequency;
+            public int Cores;
+            public bool HasIntegratedGraphics;
+            public int Year;
+            public int Cache;
+
+            public double CalculatePerformance()
+            {
+                return Frequency * Cores;
+            }
+        }
+
+    }
+}
